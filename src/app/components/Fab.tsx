05bb6d9269607acc280b4c88f5c3c672db29de41
JENkt4k <james.e.nelson@gmail.com>
2025-09-02 20:15:50 -0500

    fab-row aria warning

--- src/app/components/Fab.tsx
+++ src/app/components/Fab.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 export default function Fab({ showInstall, onNew, onPrint, onInstall, onGear, showGear }: Props) {
   return (
-    <div className="fab-row" aria-hidden>
+    <div className="fab-row" >
       {showGear && <button className="btn btn-primary fab" title="Show controls" onClick={onGear}>⚙️</button>}
       <button className="btn fab" title="New" onClick={onNew}>↻</button>
       <button className="btn fab" title="Print" onClick={onPrint}>🖨️</button>
