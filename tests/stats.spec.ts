ebd1e055975ac971b1ee1986c9b6d9a92d2c17f9
JENkt4k <james.e.nelson@gmail.com>
2025-09-05 20:30:48 -0500

    tests pass since calibraction calc changed, updates to maze creation will change this value again

--- tests/stats.spec.ts
+++ tests/stats.spec.ts
@@ -3,5 +3,5 @@ import { createMaze } from "@src/app/maze";
 test("stats are stable", () => {
   const { stats } = createMaze({ width:19, height:19, seed:42, g:0.3, b:0.15, tau:0.4 });
   expect(stats).toEqual({ L: expect.any(Number), T: expect.any(Number), J: expect.any(Number), E: expect.any(Number), D:  expect.any(Number) });
-  expect(stats.D).toBeCloseTo(5.185, 3); // replace with the known-good number from main
+  expect(stats.D).toBeCloseTo(6.499, 3); //5.185, 3); // replace with the known-good number from main
 });
